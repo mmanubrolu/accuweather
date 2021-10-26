b7521539c54333e895cb2b71d908c4946a1efa13
mmanubrolu <mmanubrolu@gmail.com>
2021-10-26 11:35:31 +0530

    text cases added for maximum temperature, minimum temperatur, screenshot as well

--- accuweather/src/main/java/com/accuweather/page/HomePage.java
+++ accuweather/src/main/java/com/accuweather/page/HomePage.java
@@ -1,7 +1,17 @@
 package com.accuweather.page;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
@@ -17,6 +27,9 @@ public class HomePage extends TestBase {
 	
 	@FindBy(xpath="//input[@class='search-input']")
 	WebElement searchBox;
+	
+	@FindBy(xpath="//span[text()='Monthly']")
+	WebElement monthly;
 	
 	// constructor
 	public HomePage() {
@@ -35,6 +48,7 @@ public class HomePage extends TestBase {
 	public String getTemperature(String city) throws InterruptedException {
 		String temperature = null;
 		searchBox.sendKeys(city);
+		Thread.sleep(4000);
 		Actions actions= new Actions(driver);
 		actions.moveToElement(searchBox);
 		actions.sendKeys(Keys.ARROW_DOWN).build().perform();
@@ -42,5 +56,138 @@ public class HomePage extends TestBase {
 		Thread.sleep(20000);
 		temperature = driver.findElement(By.xpath("(//div[@class='temp'])[1]")).getText();
 		return temperature;
+	}
+	
+	public HashMap<Integer, String> getHighestTempList(String city) throws InterruptedException{
+		HashMap<Integer, String > tempMap = new HashMap<Integer, String>();
+		
+		searchBox.sendKeys(city);
+		Thread.sleep(4000);
+		Actions actions= new Actions(driver);
+		actions.moveToElement(searchBox);
+		actions.sendKeys(Keys.ARROW_DOWN).build().perform();
+		actions.sendKeys(Keys.ENTER).build().perform();
+		Thread.sleep(20000);
+		
+		monthly.click();
+		Thread.sleep(20000);
+		
+		driver.findElement(By.xpath("//h2[text()='October']")).click();
+		Thread.sleep(10000);
+		driver.findElement(By.xpath("//span[text()='September']")).click();
+		Thread.sleep(10000);
+		
+		tempMap.put(1, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'1')])[2]/../..//div[@class='high  ']")).getText());
+		tempMap.put(2, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'2')])[2]/../..//div[@class='high  ']")).getText());
+		tempMap.put(3, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'3')])[3]/../..//div[@class='high  ']")).getText());
+		tempMap.put(4, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'4')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(5, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'5')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(6, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'6')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(7, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'7')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(8, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'8')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(9, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'9')])[2]/../..//div[@class='high  ']")).getText());
+		tempMap.put(10, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'10')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(11, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'11')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(12, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'12')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(13, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'13')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(14, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'14')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(15, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'15')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(16, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'16')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(17, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'17')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(18, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'18')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(19, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'19')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(20, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'20')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(21, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'21')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(22, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'22')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(23, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'23')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(24, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'24')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(25, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'25')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(26, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'26')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(27, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'27')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(28, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'28')])[1]/../..//div[@class='high  ']")).getText());
+		tempMap.put(29, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'29')])[2]/../..//div[@class='high  ']")).getText());
+		tempMap.put(30, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'30')])[2]/../..//div[@class='high  ']")).getText());
+		
+		System.out.println("high temperature map ::" + tempMap);
+		return tempMap;
+		
+		
+	}
+	
+	
+	public HashMap<Integer, String> getLowestTempList(String city) throws InterruptedException{
+		HashMap<Integer, String > tempMap = new HashMap<Integer, String>();
+		
+		searchBox.sendKeys(city);
+		Thread.sleep(4000);
+		Actions actions= new Actions(driver);
+		actions.moveToElement(searchBox);
+		actions.sendKeys(Keys.ARROW_DOWN).build().perform();
+		actions.sendKeys(Keys.ENTER).build().perform();
+		Thread.sleep(20000);
+		
+		monthly.click();
+		Thread.sleep(20000);
+		
+		driver.findElement(By.xpath("//h2[text()='October']")).click();
+		Thread.sleep(10000);
+		driver.findElement(By.xpath("//span[text()='September']")).click();
+		Thread.sleep(10000);
+		
+		tempMap.put(1, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'1')])[2]/../..//div[@class='low']")).getText());
+		tempMap.put(2, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'2')])[2]/../..//div[@class='low']")).getText());
+		tempMap.put(3, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'3')])[3]/../..//div[@class='low']")).getText());
+		tempMap.put(4, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'4')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(5, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'5')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(6, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'6')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(7, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'7')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(8, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'8')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(9, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'9')])[2]/../..//div[@class='low']")).getText());
+		tempMap.put(10, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'10')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(11, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'11')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(12, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'12')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(13, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'13')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(14, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'14')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(15, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'15')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(16, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'16')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(17, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'17')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(18, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'18')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(19, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'19')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(20, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'20')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(21, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'21')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(22, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'22')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(23, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'23')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(24, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'24')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(25, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'25')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(26, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'26')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(27, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'27')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(28, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'28')])[1]/../..//div[@class='low']")).getText());
+		tempMap.put(29, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'29')])[2]/../..//div[@class='low']")).getText());
+		tempMap.put(30, driver.findElement(By.xpath("(//div[@class='date' and contains(text(),'30')])[2]/../..//div[@class='low']")).getText());
+		
+		System.out.println("Lowest temperature map ::" + tempMap);
+		return tempMap;
+		
+		
+	}
+	public void captureScreenshot(String city) throws InterruptedException, IOException {
+		searchBox.sendKeys(city);
+		Thread.sleep(4000);
+		Actions actions= new Actions(driver);
+		actions.moveToElement(searchBox);
+		actions.sendKeys(Keys.ARROW_DOWN).build().perform();
+		actions.sendKeys(Keys.ENTER).build().perform();
+		Thread.sleep(10000);
+		
+		monthly.click();
+		Thread.sleep(10000);
+		
+		TakesScreenshot takeScreenShot = (TakesScreenshot) driver;
+		File sourceFile = takeScreenShot.getScreenshotAs(OutputType.FILE);
+		File tagetFile = new File("C:\\Users\\malli\\workspace\\accuweather\\logs\\accuweathersnap.jpg");
+		FileUtils.copyFile(sourceFile, tagetFile);
+		
+		Thread.sleep(10000);
+		
 	}
 }
--- accuweather/src/test/java/com/accuweather/test/HomePageTest.java
+++ accuweather/src/test/java/com/accuweather/test/HomePageTest.java
@@ -1,5 +1,10 @@
 package com.accuweather.test;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -38,6 +43,100 @@ public class HomePageTest extends TestBase {
 			e.printStackTrace();
 		}
 		System.out.println("City :: " + prop.getProperty("city") + "and its temperature ::" + temperature);
+	}
+	
+	@Test(priority=3)
+	public void validateHighestTemperatureTest() {
+		
+		try {
+			 HashMap<Integer, String> map= homePage.getHighestTempList(prop.getProperty("city"));
+			 System.out.println("high temperature map ::" + map);
+			 
+			 Iterator<Entry<Integer,String>> iterator = map.entrySet().iterator();
+			 
+			 Entry<Integer,String> firtstItem = iterator.next();
+			 Integer key =firtstItem.getKey();
+			 String val= firtstItem.getValue();
+			 
+			 while(iterator.hasNext()) {
+				 Entry<Integer,String> item = iterator.next();
+				 Integer tempKey= item.getKey();
+				 String tempVal=item.getValue();
+				 int i= tempVal.compareTo(val);
+				 if(i==1) {
+					 key=tempKey;
+					 val= tempVal;
+				 }
+				
+			 }
+			 System.out.println("Highest temperature ::  " + val);
+			 System.out.println("Highest temperature days::  " );
+			 Iterator<Entry<Integer,String>> iterator1 = map.entrySet().iterator();
+			 while(iterator1.hasNext()) {
+				 Entry<Integer,String> item = iterator1.next();
+				 if(val.trim()==item.getValue().trim()) {
+					 System.out.println("    " + item.getKey());
+				 }
+				
+			 }
+			 
+			 
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Test(priority=4)
+	public void validateLowestTemperatureTest() {
+		
+		try {
+			 HashMap<Integer, String> map= homePage.getLowestTempList(prop.getProperty("city"));
+			 System.out.println("Lowest temperature map ::" + map);
+			 
+			 Iterator<Entry<Integer,String>> iterator = map.entrySet().iterator();
+			 
+			 Entry<Integer,String> firtstItem = iterator.next();
+			 Integer key =firtstItem.getKey();
+			 String val= firtstItem.getValue();
+			 
+			 while(iterator.hasNext()) {
+				 Entry<Integer,String> item = iterator.next();
+				 Integer tempKey= item.getKey();
+				 String tempVal=item.getValue();
+				 int i= tempVal.compareTo(val);
+				 if(i== -1) {
+					 key=tempKey;
+					 val= tempVal;
+				 }
+				
+			 }
+			 System.out.println("Lowest temperature ::  " + val);
+			 System.out.println("Lowest temperature days::  " );
+			 Iterator<Entry<Integer,String>> iterator1 = map.entrySet().iterator();
+			 while(iterator1.hasNext()) {
+				 Entry<Integer,String> item = iterator1.next();
+				 if(val.trim()==item.getValue().trim()) {
+					 System.out.println("    " + item.getKey());
+				 }
+				
+			 }
+			 
+			 
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test(priority=5)
+	public void validateScreenshotTest() {
+		try {
+			homePage.captureScreenshot(prop.getProperty("city"));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	@AfterMethod
 	public void tearDown() {
